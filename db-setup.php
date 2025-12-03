97eeb5a268b426441c8a679daca3a9d7b18c68a0
Nico Christmann <77099168+Happynico7504@users.noreply.github.com>
2025-12-03 10:36:00 +0100

    Update db-setup.php

--- db-setup.php
+++ db-setup.php
@@ -10,7 +10,7 @@ $dbname="your_dbname";
 $db_handle = new mysqli($hostname, $username, $password, $dbname);
 
 if (!mysqli_connect_error()) {
-  $field_empty_check = $db_handle->query("SHOW TABLES;");
+  $field_empty_check = $db_handle->query("SHOW TABLES in $dbname;");
   if($field_empty_check == false) {
     $table_setup = $db_handle->query("CREATE TABLE IF NOT EXISTS `stats` (`hits` INT(255) NOT NULL , `misses` INT(255) NOT NULL ) ENGINE = InnoDB;");
     $field_setup = $db_handle->query("INSERT INTO `stats` (`hits`, `misses`) VALUES (0, 0)");
