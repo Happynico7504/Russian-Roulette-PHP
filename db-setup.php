45c0bdebb70782957e9c595a03485c7c112e09b2
Nico Christmann <77099168+Happynico7504@users.noreply.github.com>
2025-12-03 09:57:53 +0100

    Change INT size in stats table from 65536 to 255

--- db-setup.php
+++ db-setup.php
@@ -10,7 +10,7 @@ $dbname="your_dbname";
 $db_handle = new mysqli($hostname, $username, $password, $dbname);
 
 if (!mysqli_connect_error()) {
-  $table_setup = $db_handle->query("CREATE TABLE IF NOT EXISTS `stats` (`hits` INT(65536) NOT NULL , `misses` INT(65536) NOT NULL ) ENGINE = InnoDB;");
+  $table_setup = $db_handle->query("CREATE TABLE IF NOT EXISTS `stats` (`hits` INT(255) NOT NULL , `misses` INT(255) NOT NULL ) ENGINE = InnoDB;");
 }
 
 ?>
