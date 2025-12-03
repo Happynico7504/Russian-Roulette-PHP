fc07cd623573af85054220bda2a477bc6d9400d3
Nico Christmann <77099168+Happynico7504@users.noreply.github.com>
2025-12-03 09:38:56 +0100

    Fix SQL syntax for inserting stats into the database

--- stats-engine.php
+++ stats-engine.php
@@ -16,7 +16,7 @@ if (!mysqli_connect_error()) {
     break;
   }
   echo "<br><br>Hits: $hits<br><br>Misses: $misses";
-  $update_stats = $db_handle->query("INSERT INTO Table (stats) VALUES ($hits, $misses)");
+  $update_stats = $db_handle->query("INSERT INTO Table `stats` (`hits`, `misses`) VALUES ($hits, $misses)");
 }
 
 ?>
