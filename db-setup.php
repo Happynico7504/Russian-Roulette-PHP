c6e30df2773535b7befa8416510f03a5cb43089e
Nico Christmann <77099168+Happynico7504@users.noreply.github.com>
2025-12-03 08:18:12 +0100

    Remove database prefix from stats table creation

--- db-setup.php
+++ db-setup.php
@@ -10,7 +10,7 @@ $dbname="your_dbname";
 $db_handle = new mysqli($hostname, $username, $password, $dbname);
 
 if (!mysqli_connect_error()) {
-  $table_setup = $db_handle->query("CREATE TABLE IF NOT EXISTS `russianroulette`.`stats` (`hits` INT(0) NOT NULL , `misses` INT(0) NOT NULL ) ENGINE = InnoDB;");
+  $table_setup = $db_handle->query("CREATE TABLE IF NOT EXISTS `stats` (`hits` INT(0) NOT NULL , `misses` INT(0) NOT NULL ) ENGINE = InnoDB;");
 }
 
 ?>
