5b59d059d8b82220c73d826508c5f0ed6e77b81b
Nico Christmann <77099168+Happynico7504@users.noreply.github.com>
2025-12-03 10:12:33 +0100

    Fix SQL syntax for 'misses' column in stats table

--- db-setup.php
+++ db-setup.php
@@ -10,7 +10,7 @@ $dbname="your_dbname";
 $db_handle = new mysqli($hostname, $username, $password, $dbname);
 
 if (!mysqli_connect_error()) {
-  $table_setup = $db_handle->query("CREATE TABLE IF NOT EXISTS `stats` (`hits` INT(255) NOT NULL DEFAULT 0 , `misses` INT(255) NOT 0 DEFAULT 0 ) ENGINE = InnoDB;");
+  $table_setup = $db_handle->query("CREATE TABLE IF NOT EXISTS `stats` (`hits` INT(255) NOT NULL DEFAULT 0 , `misses` INT(255) NOT NULL DEFAULT 0 ) ENGINE = InnoDB;");
 }
 
 ?>
